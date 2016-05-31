d2ba54aff5c764a256373c474b6c67465130f216
GaryDix <gary.d@visioncollege.ac.nz>
2016-05-31 15:06:02 +1200

    Monday2

--- OCRRecognition/MainActivity.cs
+++ OCRRecognition/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿//https://www.microsoft.com/cognitive-services/en-us/computer-vision-api/documentation/ocr 
 //https://dev.projectoxford.ai/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1fa
 using System;
+using System.IO;
 using Android.App;
 using Android.Content;
 using Android.Runtime;
@@ -14,15 +15,22 @@ using System.Threading.Tasks;
 using System.Web;
 using Android.Graphics;
 using Android.Media;
-using Java.IO;
+using Android.Util;
 using Encoding = System.Text.Encoding;
+using File = Java.IO.File;
 
-namespace OCRRecognition {
+namespace OCRRecognition
+{
     [Activity(Label = "OCRRecognition", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity {
+    public class MainActivity : Activity
+    {
         private static TextView ResultText;
         private ImageView _imageView;
-        protected override void OnCreate(Bundle bundle) {
+        private string path;
+        private string filepath;
+        private static string tag = "aaaa";
+        protected override void OnCreate(Bundle bundle)
+        {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
@@ -35,34 +43,40 @@ namespace OCRRecognition {
             ResultText = FindViewById<TextView>(Resource.Id.TextResult);
             loadImage.Click += LoadImage;
             sendImage.Click += SendImage;
-            }
-        private void LoadImage(object sender, EventArgs e) {
+
+            path = new File(
+             Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures), "OCR").AbsolutePath.ToString();
+            filepath = System.IO.Path.Combine(path, "test.jpg");
+        }
+        private void LoadImage(object sender, EventArgs e)
+        {
             //https://developer.xamarin.com/guides/xamarin-forms/working-with/images/
             StartActivity(typeof(Gallery));
             // var beachImage = new Image { Aspect = Aspect.AspectFit };
             // beachImage.Source = ImageSource.FromFile("waterfront.jpg");
 
 
-            }
-        private async void SendImage(object sender, EventArgs e) {
+        }
+        private async void SendImage(object sender, EventArgs e)
+        {
             //if the app is there load it 
             Bitmap bitmap = await GetImage();
             _imageView.SetImageBitmap(bitmap);
 
 
-
-            MakeRequest(bitmap);
-            }
+            MakeRequest(filepath);
 
 
-        async Task<Bitmap> GetImage() {
-            var path = new File(
-                Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures), "OCR").AbsolutePath.ToString();
-            var filePath = System.IO.Path.Combine(path, "test.jpg");
+        }
+
+
+        async Task<Bitmap> GetImage()
+        {
+
 
             //true if the caller has the required permissions and path contains the name of an existing file; otherwise, false. This method also returns false if path is null, an invalid path, or a zero-length string.
             //if (System.IO.File.Exists(filePath)) {
-            Bitmap bitmap = await BitmapFactory.DecodeFileAsync(filePath);
+            Bitmap bitmap = await BitmapFactory.DecodeFileAsync(filepath);
             // _imageView.SetImageBitmap(bitmap);
             return bitmap;
             //   bitmap.Recycle();
@@ -73,39 +87,77 @@ namespace OCRRecognition {
             //Toast.MakeText(this, "Nope, no image here", ToastLength.Long).Show();
             //return null;
             //}
-            }
+        }
 
         //https://dev.projectoxford.ai/docs/services/56f91f2d778daf23d8ec6739/operations/56f91f2e778daf14a499e1fc/console 
 
-        static async void MakeRequest(Bitmap bitmap) {
+        static async void MakeRequest(string ImageFilePath)
+        {
 
-           // var uri = Android.Net.Uri.Parse("PathToYourResource"); 
-            var client = new HttpClient();
+            // var uri = Android.Net.Uri.Parse("PathToYourResource"); 
+            var Httpclient = new HttpClient();
             var queryString = HttpUtility.ParseQueryString(string.Empty);
 
             // Request headers
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{}");
+            Httpclient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "");
 
+            //http://javatechig.com/xamarin/upload-bitmap-image-to-server-using-http-multipart-in-xamarin-android 
             // Request parameters
             queryString["language"] = "unk";
             queryString["detectOrientation "] = "true";
             var uri = "https://api.projectoxford.ai/vision/v1.0/ocr?" + queryString;
-
+            Log.Info(tag, "uri " + uri);
             HttpResponseMessage response;
 
             // Request body
-           // Bitmap image = Bitmap.CreateBitmap((Bitmap)Resource.Drawable.sign);
-            byte[] byteData = Encoding.UTF8.GetBytes("{bitmap}");
+            // Bitmap image = Bitmap.CreateBitmap((Bitmap)Resource.Drawable.sign);
 
-            using (var content = new ByteArrayContent(byteData)) {
-                content.Headers.ContentType = new MediaTypeHeaderValue("< application/json >");
-                response = await client.PostAsync(uri, content);
+            string url =
+                "http://lh4.ggpht.com/_gKQKwLZ8XUs/TAevIEb8FkI/AAAAAAAAC3w/8kMg7Yze__Q/s800/Funny-Signs-Sharp-45.jpg";
+            byte[] byteData = Encoding.UTF8.GetBytes(url);
+
+            using (var fileContent = new ByteArrayContent(byteData))
+            {
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue("< application/json >");
+
+                Log.Info(tag, "content " + fileContent);
+                //gets sent here with uri and content
+                response = await Httpclient.PostAsync(uri, fileContent);
                 ResultText.Text = response.ToString();
 
-                }
-
             }
+
         }
 
+        public async Task<String> UploadBitmapAsync(Bitmap bitmap)
+        { //http://javatechig.com/xamarin/upload-bitmap-image-to-server-using-http-multipart-in-xamarin-android
+            byte[] bitmapData;
+            var stream = new MemoryStream();
+            bitmap.Compress(Bitmap.CompressFormat.Jpeg, 0, stream);
+            bitmapData = stream.ToArray();
+            var fileContent = new ByteArrayContent(bitmapData);
+
+            fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/octet-stream");
+            fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            {
+                Name = "file",
+                FileName = "my_uploaded_image.jpg"
+            };
+
+            string boundary = "---8d0f01e6b3b5dafaaadaad";
+            MultipartFormDataContent multipartContent = new MultipartFormDataContent(boundary);
+            multipartContent.Add(fileContent);
+            string UPLOAD_IMAGE = "http://YOUR_SERVER/api/poi/upload";
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.PostAsync(UPLOAD_IMAGE, multipartContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            return null;
+        }
     }
+
+}
 
